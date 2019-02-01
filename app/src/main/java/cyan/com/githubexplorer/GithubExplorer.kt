7e6391d57c56905da64e74bc5ae74133d84e09af
SpiderMurphy <vincenzocaleca@gmail.com>
2019-02-01 19:28:09 +0100

    Added Application component and completed dependencies graph

--- app/src/main/java/cyan/com/githubexplorer/GithubExplorer.kt
+++ app/src/main/java/cyan/com/githubexplorer/GithubExplorer.kt
@@ -1,10 +1,11 @@
 package cyan.com.githubexplorer
 
+import cyan.com.githubexplorer.di.component.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class GithubExplorer : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out GithubExplorer> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return DaggerApplicationComponent.builder().application(this).build()
     }
 }
